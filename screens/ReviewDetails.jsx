54cc6415d629427f4025cc5ee92621186b47f026
QDev2k3 <ntbquoc2015@gmail.com>
2023-08-15 21:44:00 +0700

    Update to display user rating

--- screens/ReviewDetails.jsx
+++ screens/ReviewDetails.jsx
@@ -3,12 +3,13 @@ import { Text, View, StyleSheet, Button, Image, StatusBar, FlatList, RefreshCont
 import { globalStyles } from '../styles/global';
 import Rating from '../components/Rating';
 import Post from '../components/Post';
-import { db, collection, getDocs } from '../firebase/index';
+import { db, collection, getDocs, getDoc, doc } from '../firebase/index';
 
 const imageBG = 'https://firebasestorage.googleapis.com/v0/b/moviereview-ca8ef.appspot.com/o/main_BG_alt.png?alt=media&token=16ae4c6d-9113-492b-8a3a-1e4bb57cad0a';
 
 export default function ReviewDetails( {route, navigation} ) {
     const { movieTitle, movieRating, moviePoster, movieID } = route.params;
+    const [ rating, setRating] = useState(movieRating);
     const [reviews, setReviews] = useState([]);
     const [IDs, setIDs] = useState([]);
     const [refreshing, setRefreshing] = useState(false);
@@ -27,6 +28,11 @@ export default function ReviewDetails( {route, navigation} ) {
         }
     }
 
+    const updateRating = async() => {
+        const docSnap = await getDoc(doc(db, "movies", movieID));
+        setRating(docSnap.data().movieRating);
+    }
+
     useEffect(() => {
         getReviews();
     }, []);
@@ -36,6 +42,7 @@ export default function ReviewDetails( {route, navigation} ) {
     const onRefresh = async () => {
         setRefreshing(true);
         await getReviews();
+        await updateRating();
         setRefreshing(false);
       };
 
@@ -47,7 +54,7 @@ export default function ReviewDetails( {route, navigation} ) {
                     <Image source={{uri: moviePoster}} style={styles.posterImage} />
                     <View style={styles.titleContainer}>
                         <Text style={globalStyles.titleText}>{movieTitle}</Text>
-                        <Rating value={movieRating} />
+                        <Rating value={rating} />
                     </View>
                 </View>
                 <Text style={{color: '#fff', fontSize: 18, fontFamily: 'nunito-regular', textAlign: 'center', marginBottom: 10,}}>User Reviews</Text>
@@ -55,13 +62,15 @@ export default function ReviewDetails( {route, navigation} ) {
                     contentContainerStyle={styles.reviewContainer}
                     data={reviews}
                     renderItem={({item}) => (
-                        <Post user={item.reviewUser} content={item.reviewPost} />
+                        <Post user={item.reviewUser} content={item.reviewPost} rating={item.reviewRating} />
                     )}
                     refreshControl={
                         <RefreshControl refreshing={refreshing} onRefresh={onRefresh} tintColor='white' color='white' />
                     }
                 />
-                <Button title='Create a review' onPress={() => navigation.navigate('PostCreate', {movieID: movieID})} />
+                <View style={{paddingHorizontal: 50,}}>
+                    <Button title='Create a review' onPress={() => navigation.navigate('PostCreate', {movieID: movieID, revNum: reviews.length})} />
+                </View>
             </ImageBackground>
         </View>
     )
