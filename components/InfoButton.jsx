0165099602522b69fd66ee5273aff06b3d8313d0
QDev2k3 <ntbquoc2015@gmail.com>
2023-09-01 08:07:04 +0700

    Change to make the component more reusable

--- components/InfoButton.jsx
+++ components/InfoButton.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { TouchableOpacity, Appearance } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 
-export default function InfoButton({func}) {
+export default function InfoButton({type, func}) {
     return (
         <TouchableOpacity onPress={func}>
-            <AntDesign name='infocirlceo' size={24} style={{
+            <AntDesign name={type} size={25} style={{
                 color:  Appearance.getColorScheme() === 'dark' ? '#000000' : '#fff',
             }} />
         </TouchableOpacity>
